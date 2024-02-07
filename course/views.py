0ae3d7cc949b61682ef6dec24532f325ce99487e
marsalan06 <arsalan.9798@gmail.com>
2024-02-07 10:50:56 +0500

    filter for organization on programs added

--- course/views.py
+++ course/views.py
@@ -31,7 +31,6 @@ class ProgramFilterView(FilterView):
     template_name = "course/program_list.html"
 
     def get_context_data(self, **kwargs):
-        print("======oooo====")
         context = super().get_context_data(**kwargs)
         context["title"] = "Programs"
         return context
